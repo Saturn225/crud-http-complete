ac97c8157b28f203d6ab69f87dc13ee64fd9e402
KoraboinaSankeerth <koraboinasankeerth07@gmail.com>
2025-11-15 21:18:17 +0530

    Updated folder name Routes to routes and added logs

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const Product = require("./models/product.model.js");
+// const Product = require("./models/product.model.js");
 const productRoute = require("./routes/product.route.js");
 const app = express();
+
+const port = process.env.PORT || 3000;
+console.log('hi');
+
 
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+console.log("Router loading...");
 
 // routes
 app.use("/api/products", productRoute);
